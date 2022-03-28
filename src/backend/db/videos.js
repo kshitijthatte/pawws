aa8f6c6e036460c8f3a78cab736c32752677ca0e
Kshitij Thatte <kshitijthatte@gmail.com>
2022-03-28 23:34:43 +0530

    chore: added data to DB

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,154 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    _id: uuid(),
+    title: "Cat eating grass",
+    thumbnail:
+      "https://image.mux.com/Jo25ox1ibDEFheB66TEtO40101gLmTRjBn/thumbnail.jpg?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InQ5UHZucm9ZY0hQNjhYSmlRQnRHTEVVSkVSSXJ0UXhKIn0.eyJ3aWR0aCI6NjQwLCJ0aW1lIjowLCJleHAiOjMxNzE5MjI1MTExMywiYXVkIjoidCIsInN1YiI6IkpvMjVveDFpYkRFRmhlQjY2VEV0TzQwMTAxZ0xtVFJqQm4ifQ.ZY5IgBGbwlP6lF9J24UgJrBMp7AFtisXRSEJ7on0SHNy5vp9HJ5xGYeIuAUvPeXUN16BmskbVsJssHphIk84ThCApGRfrgMeIBlGIrM5J0Xcy4RIR3yh6Et-GbI5Tc2bW3icdMh7VOau99cpMfD97V4M0QPjMqaYPcIuIeLmi2L6s5jldWVqbCvJlHoDg0Z_kE7tKNZZPuAMfaAK6essWkkQevxuZk27DukK7TZGzeIEOyI54EOvH4s6pX0uPbKZXxLzIOiZWgnBRxWJ7mU2BxqCV6cFyXmLHF6JtvUo842X_4mVNmEjdOCv_Bz19POT40zrCsod2jR_gMQ87mQ8-g",
+    description: "Still shot of a cat eating grass in the garden. ",
+    views: 5032,
+    user: "Michal Scarn",
+    category: "Cat",
+    src: "https://storage.coverr.co/videos/Jo25ox1ibDEFheB66TEtO40101gLmTRjBn/preview?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjRDQzlGMzk4NEU5NzdCMkJDQUMwIiwiaWF0IjoxNjQ4MzI1NzgwfQ.uqeLSw6R49jDSO5S76-ITa5xQiPmVGjyHgfK0kRY20A",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: uuid(),
+    title: "Black kitten cleaning its paws",
+    thumbnail:
+      "https://image.mux.com/LrWVZ6w9yx00GDtQ02chOsZ1FblQg4IVWw/thumbnail.jpg?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InQ5UHZucm9ZY0hQNjhYSmlRQnRHTEVVSkVSSXJ0UXhKIn0.eyJ3aWR0aCI6NjQwLCJ0aW1lIjowLCJleHAiOjMxNzE5MjI1MDg1OSwiYXVkIjoidCIsInN1YiI6IkxyV1ZaNnc5eXgwMEdEdFEwMmNoT3NaMUZibFFnNElWV3cifQ.iS2ZN9OM9sAaYOj0WOl7BC5iEKlw6wwrs0k6rEatFmCgC8yodDiy8HTX_6OBEeeJZ2v6fb1OyxPqDLK_6ZW5uregv3Zo01Bx_a3F7UhRR-r2jK22BT2PWy7du0HebOeHs51_EqIqgkVQXDwNa3fb5WXB_CFKxMBarMJPZsoT3bsewPDV9V7e28vL3GjDeOYWvxyDfZIRNCTjlvdP9SmZ1rE4Y89A1XKA4dOE27E8h9lpfW94Cd4wAyN4oSnXqBD6Y8l9JnBK4AUFScGOmMrU95diTknQAfj0pWNLEG3ykRUboePwL59zx3zFesaMRrQhLoFDJ4Iul11F1nzt1ltFEg",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Close-up shot of an adorable, black kitten cleaning its paws. ",
+    views: "1113",
+    user: "Carl",
+    category: "Cat",
+    src: "https://storage.coverr.co/videos/LrWVZ6w9yx00GDtQ02chOsZ1FblQg4IVWw/preview?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjRDQzlGMzk4NEU5NzdCMkJDQUMwIiwiaWF0IjoxNjQ4MzI1NzgwfQ.uqeLSw6R49jDSO5S76-ITa5xQiPmVGjyHgfK0kRY20A",
+  },
+
+  {
+    _id: uuid(),
+    title: "Cat at the breakfast table",
+    thumbnail:
+      "https://image.mux.com/Vcve11Kp8bptZ00n1nsBEOh02qF5fgShYW/thumbnail.jpg?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InQ5UHZucm9ZY0hQNjhYSmlRQnRHTEVVSkVSSXJ0UXhKIn0.eyJ3aWR0aCI6NjQwLCJ0aW1lIjowLCJleHAiOjMxNzE5MjI1MTE5OSwiYXVkIjoidCIsInN1YiI6IlZjdmUxMUtwOGJwdFowMG4xbnNCRU9oMDJxRjVmZ1NoWVcifQ.Tjd_SJ-TdypCjn0ruoD1zpiW4iQgE6oX9HW3KwOMVB4lO0pyuyW877R2dNLvGLdCd83tKqhCtFGJIwJffGW9pVKtWC6v147PemzopvBdbWw19j3c9xLKX1sACRpt9hPCmY5U3cYqTqCiU9zdRWsdhnudeV1AAnuRFJDyufJ6F4gjW_lqIotTttgT74Rck8EAwOR62OqgH6qQMIBaYyt5WdYxSiW92RcyUVSBcfYXTagX3RNISTYcKVvC9BUHjxILyJwrMs--Sg8FNezEl8DcUiFy0sanaqpDTjfnhaeYntyRpxIcjs8bba-cidpl1agolLFQW7ixNVr67HqPnu1cLA",
+
+    description:
+      "A mid shot of a cat sitting at the breakfast table trying to eat some banana pancakes. ",
+    views: "5603",
+    user: "Jhon Doe",
+    category: "Cat",
+    src: "https://storage.coverr.co/videos/Vcve11Kp8bptZ00n1nsBEOh02qF5fgShYW/preview?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjRDQzlGMzk4NEU5NzdCMkJDQUMwIiwiaWF0IjoxNjQ4MzI1NzgwfQ.uqeLSw6R49jDSO5S76-ITa5xQiPmVGjyHgfK0kRY20A",
+  },
+  {
+    _id: uuid(),
+    title: "Cat in the grass",
+    thumbnail:
+      "https://image.mux.com/XRqHFNqz3ZOx8FzqrGUNNUUz1m01Cm3jK/thumbnail.jpg?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InQ5UHZucm9ZY0hQNjhYSmlRQnRHTEVVSkVSSXJ0UXhKIn0.eyJ3aWR0aCI6NjQwLCJ0aW1lIjowLCJleHAiOjMxNzE5MjI1MTE0NSwiYXVkIjoidCIsInN1YiI6IlhScUhGTnF6M1pPeDhGenFyR1VOTlVVejFtMDFDbTNqSyJ9.lmgnFjdHUNA9b27Rbffwim2Co0uTsqfwAWSk_JQhZX4fhlsal7DsUp5XX0GHuYBxw0-5T4fzElyY_f4HQ-fusYi7CG4cIochghJ9W9xfnbzSwlDIznm7Xkhc7tONb86tBGkWMRUDCC-tU55s8cbHsJHUYBzTKrJiscJRSQvbBVBz1AgO4rrCi95LYpmxn4O2J_vy6SkNuzXAdg9JF5rKEU2YGniaolvzSM_oCKfpJN8x1NKm0AgR6q_ujCrz2yaOYUA1B9Y2MJyhOoalNDW7RQ59SS1qgD5yzQCdhwNFOgCkm_o0txhWV2BCVu7iHLLyJm1XxKT45GvYlZSpgJc-yA",
+
+    description:
+      "Mid-shot of a cat sitting in the grass and observing its surroundings. ",
+    views: "2632",
+    user: "Regina Phalange",
+    category: "Cat",
+    src: "https://storage.coverr.co/videos/XRqHFNqz3ZOx8FzqrGUNNUUz1m01Cm3jK/preview?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjRDQzlGMzk4NEU5NzdCMkJDQUMwIiwiaWF0IjoxNjQ4MzI1NzgwfQ.uqeLSw6R49jDSO5S76-ITa5xQiPmVGjyHgfK0kRY20A",
+  },
+  {
+    _id: uuid(),
+    title: "Cat sitting on the street",
+    thumbnail:
+      "https://image.mux.com/ZWI9vdPcw313z01CcaqH5NGhMJ6WlB023o/thumbnail.jpg?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InQ5UHZucm9ZY0hQNjhYSmlRQnRHTEVVSkVSSXJ0UXhKIn0.eyJ3aWR0aCI6NjQwLCJ0aW1lIjowLCJleHAiOjMxNzE5MjI1MTUwNywiYXVkIjoidCIsInN1YiI6IlpXSTl2ZFBjdzMxM3owMUNjYXFINU5HaE1KNldsQjAyM28ifQ.mcRNvZaC5zj0vdLmbs1mfqVSUAaE992qw3eGSAcfWxfb96U_HGn1C636sZASVmF9A4nmoHxFa7b1P2BbGmwofWK8ojuGexeHBxdXOVNdLncUGRwWKjMupt3kmT3DtOUlDL9CriVdAGRSoYLAlPR9TgbC-RIh5NjYC8t26dPWR4Ck3swEA5LAJFTyOevOQyLQ-6iArucoStxKmV4FsQB7e_KE3bdCVhtk0kVxl_SrekNfPVKg7cj3HBxA7Kqhs3cmILdAFeeGL9mXqH52HIGppBHU6oaq4RHDtVOQwtRmgjzX6L1XmqLMjQJC5kKQp9rdZnEJPEpTQT-oh_NlOL5y6w",
+
+    description:
+      "A still shot of a grey cat sitting on the street as a couple walks in the background. \n",
+    views: "7256",
+    user: "Michael Scott",
+    category: "Cat",
+    src: "https://storage.coverr.co/videos/ZWI9vdPcw313z01CcaqH5NGhMJ6WlB023o/preview?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjRDQzlGMzk4NEU5NzdCMkJDQUMwIiwiaWF0IjoxNjQ4MzI1NzgwfQ.uqeLSw6R49jDSO5S76-ITa5xQiPmVGjyHgfK0kRY20A",
+  },
+  {
+    _id: uuid(),
+    title: "Cat standing on a table",
+    thumbnail:
+      "https://image.mux.com/f00VX012PFAXzRJ4b8sthHcWZ02jstACYhS/thumbnail.jpg?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InQ5UHZucm9ZY0hQNjhYSmlRQnRHTEVVSkVSSXJ0UXhKIn0.eyJ3aWR0aCI6NjQwLCJ0aW1lIjowLCJleHAiOjMxNzE5MjI1MDg4MywiYXVkIjoidCIsInN1YiI6ImYwMFZYMDEyUEZBWHpSSjRiOHN0aEhjV1owMmpzdEFDWWhTIn0.usLOoXWYyw9_SYT-VEigt5FI_b0gyHF2Ehq6aDIKI4GGoU-VrUYplZtca9rTyKqIsuP0q9lHz8jxNQqHMPxhWCDxaFcSgHa__51MQVpzHqRYUsTXRmEJXZkDeKROqI8eGKk3nY4VTQsEgf6jNU_nQEDEDdemN4tSDqQ9p2fsM4MOYCMMlaqr5K9vGpUO0P2IdmaYca3ykU7dU84tMtjQWFF7kPTcnw85WzolrnsCUntvquVrGUD12ILQ8dEyasP-ADFc_tbD7YA5devOCcmhIrEq8NAYnxxtFZg0EbxGFaMLUKXJSAva3Mt5pZFoxRA7fbnAc2zEbwldNlEq8ZGbIQ",
+
+    description:
+      "A still shot of a cat standing up on a table waiting to eat its owner's food from a plate.",
+    views: "4521",
+    user: "Ross",
+    category: "Cat",
+    src: "https://storage.coverr.co/videos/f00VX012PFAXzRJ4b8sthHcWZ02jstACYhS/preview?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjRDQzlGMzk4NEU5NzdCMkJDQUMwIiwiaWF0IjoxNjQ4MzI1NzgwfQ.uqeLSw6R49jDSO5S76-ITa5xQiPmVGjyHgfK0kRY20A",
+  },
+  {
+    _id: uuid(),
+    title: "Cat near a woman relaxing",
+    thumbnail:
+      "https://image.mux.com/nDoj1ssScD4s1oqKGTWDja9A6CbsDMIi/thumbnail.jpg?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InQ5UHZucm9ZY0hQNjhYSmlRQnRHTEVVSkVSSXJ0UXhKIn0.eyJ3aWR0aCI6NjQwLCJ0aW1lIjowLCJleHAiOjMxNzE5MjI1MTMwMiwiYXVkIjoidCIsInN1YiI6Im5Eb2oxc3NTY0Q0czFvcUtHVFdEamE5QTZDYnNETUlpIn0.k8Aqi65jTwDRdvLHEg-uLLzEBhelsOfiwS53EYHyFuMl_Lng4Y_bT5IyOSom035fqi916SlBrTgvwC4h4AqH2USxs5Bv50DrXh9u_CRYq3kpTwKKMUAPYllAdPo4fl7cBAOTXADSkiow0v2kOCIcmWoNgSO6rysv6R9jY4ViWcNIE6_m87HK44Wb_gyV2XfTXoQGv61q9D3bD_IubD-WKD7SQwjX1tyVTueKcdaLjhIQ6znvtmbJ2wDAUgVwX2mI7pqaMiYK-bw_6QLNgGdTUbw-vKDy5CxhEbiUys8VFN5XnjsffM8gQFj1TQc1aSvqF8Loozp5zXsCw0rvyvNbvA",
+
+    description: "Still shot of a cat walking near a woman relaxing. ",
+    views: "5412",
+    user: "Steve Jobs",
+    category: "Cat",
+    src: "https://storage.coverr.co/videos/nDoj1ssScD4s1oqKGTWDja9A6CbsDMIi/preview?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjRDQzlGMzk4NEU5NzdCMkJDQUMwIiwiaWF0IjoxNjQ4MzI1NzgwfQ.uqeLSw6R49jDSO5S76-ITa5xQiPmVGjyHgfK0kRY20A",
+  },
+  {
+    _id: uuid(),
+    title: "A dog approaching a cat ",
+    thumbnail:
+      "https://image.mux.com/ZwbShoa7cHRdmoxKGWNAsRwey01hrnxYd/thumbnail.jpg?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InQ5UHZucm9ZY0hQNjhYSmlRQnRHTEVVSkVSSXJ0UXhKIn0.eyJ3aWR0aCI6NjQwLCJ0aW1lIjowLCJleHAiOjMxNzE5MjI1MTIxMSwiYXVkIjoidCIsInN1YiI6Ilp3YlNob2E3Y0hSZG1veEtHV05Bc1J3ZXkwMWhybnhZZCJ9.cDPXWAqiwa8GZjDSSgQkAwQgKQcG-ITsQnkH2_qKON9p6-H2UJRA5-5OSvSFk2b6wsRjSS_fsLlzYNJ3UELVXJPEEDfpZtMbdz1unPUkFrblYfUedhRKSgY4CDU3Mvdk4YwH4xALgBGR_3ATbVo7eEiuz8eMChfiiCxA-kWz0RbQyd_fb2oDrhCRiu11ZuoF8HbPw9aUYtHEc86HRXegrTwjPqbEIXV5QqeLVqhc0w-snflLbMoeHLXyiLGObZSGJ5VtCDTxZoQ46TSZ1_6A57hgRd-PqkzuFqkEJqLePj5erB0fpiNRbCm58HqC_qAIAt0iFDt1WpiEQMGvi-n2qQ",
+
+    description:
+      "A still shot of a dog and its owner approaching a cat sitting on a path ahead of them. ",
+    views: "9654",
+    user: "Janice",
+    category: "Cat",
+    src: "https://storage.coverr.co/videos/ZwbShoa7cHRdmoxKGWNAsRwey01hrnxYd/preview?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjRDQzlGMzk4NEU5NzdCMkJDQUMwIiwiaWF0IjoxNjQ4MzI1NzgwfQ.uqeLSw6R49jDSO5S76-ITa5xQiPmVGjyHgfK0kRY20A",
+  },
+  {
+    _id: uuid(),
+    title: "Man walking past a cat",
+    thumbnail:
+      "https://image.mux.com/ajUW00fo3YfzYphqEQSESqJx3N9401Uk00t/thumbnail.jpg?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InQ5UHZucm9ZY0hQNjhYSmlRQnRHTEVVSkVSSXJ0UXhKIn0.eyJ3aWR0aCI6NjQwLCJ0aW1lIjowLCJleHAiOjMxNzE5MjI1MTE1OSwiYXVkIjoidCIsInN1YiI6ImFqVVcwMGZvM1lmellwaHFFUVNFU3FKeDNOOTQwMVVrMDB0In0.gnXsatirv_Gw02IiBvmf8C7h34MiuIkR-XsN8nHf_m_XIVLyKxtNv9s4HxGmGVdSNVWesH6n04LrvYH-MP2iCWHdwRoV5aPznqlKmsR1LI_0ofhOm86hF1ss9UATzVRAbBWRsQOznMriLHhy5SAJgtTpJNfylpiDCrUC6ixS8Gd_KCgjird4dsz-tOJjt3qeduTs2bdoVIt5UOmUJb0tkvEiplYt4xymkaEdgDs2OuaHnOPVGCI1v-jPWFc9yyO0hzFjPLTmmPyo41NKbuOw1TsFYIKKViCxu2COKGVN1jPIxHjwECsY93aASdRs9xjlYwRmlRjC8P-Ej_wk7VZs1w",
+
+    description:
+      "A still shot of a man with a walking stick, walking past a cat. ",
+    views: "6521",
+    user: "Bob Vance",
+    category: "Cat",
+    src: "https://storage.coverr.co/videos/ajUW00fo3YfzYphqEQSESqJx3N9401Uk00t/preview?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjRDQzlGMzk4NEU5NzdCMkJDQUMwIiwiaWF0IjoxNjQ4MzI1NzgwfQ.uqeLSw6R49jDSO5S76-ITa5xQiPmVGjyHgfK0kRY20A",
+  },
+  {
+    _id: uuid(),
+    title: "Pink Chinese cat waving",
+    thumbnail:
+      "https://image.mux.com/m1vCto6Ujvo8Xa86NPS3ipI8n18AMD8I/thumbnail.jpg?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InQ5UHZucm9ZY0hQNjhYSmlRQnRHTEVVSkVSSXJ0UXhKIn0.eyJ3aWR0aCI6NjQwLCJ0aW1lIjowLCJleHAiOjMxNzE5MjI1MTEwNSwiYXVkIjoidCIsInN1YiI6Im0xdkN0bzZVanZvOFhhODZOUFMzaXBJOG4xOEFNRDhJIn0.Bkkz4jS03uzYRBS1K7AY1iEkIPnDmP7MLN34-xthiAR4kUff8izyXQ64OgOlAmSwWJOndww3X58VR2nHJYBB1fdc78p2ltsN5I9dN6sL9INv6BZIO0uOcumU_W2HQjtZn0aRS0YLlFm9JHLsLN1iprAEfsKTg19uiVE9m3mMgAXdhRfHf6PL0r0920-bP_aZbJT8hsnBwYquNmcJMY_50J-ISOgaTQJ4-8nN4_iG34v8L15nUgPIFp1xkvIZcrMr8Vq9C1cbUjDszV0PBEekr02kOlXw_Ojwz5yQGzipCVdeOcpfhzXRhxb4vcVkio2akOYaW7IrzGT2oJm8U_zZtg",
+
+    description: "A close-up shot of a pink Chinese cat waving. ",
+    views: "2156",
+    user: "Chandler Bing",
+    category: "Cat",
+    src: "https://storage.coverr.co/videos/m1vCto6Ujvo8Xa86NPS3ipI8n18AMD8I/preview?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjRDQzlGMzk4NEU5NzdCMkJDQUMwIiwiaWF0IjoxNjQ4MzI1NzgwfQ.uqeLSw6R49jDSO5S76-ITa5xQiPmVGjyHgfK0kRY20A",
+  },
+  {
+    _id: uuid(),
+    title: "A kitten yawning",
+    thumbnail:
+      "https://image.mux.com/HYiO01rDqsGLR8zHzB47pHpg6B017wPvJU/thumbnail.jpg?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InQ5UHZucm9ZY0hQNjhYSmlRQnRHTEVVSkVSSXJ0UXhKIn0.eyJ3aWR0aCI6NjQwLCJ0aW1lIjowLCJleHAiOjMxNzE5MjI5MTIxMCwiYXVkIjoidCIsInN1YiI6IkhZaU8wMXJEcXNHTFI4ekh6QjQ3cEhwZzZCMDE3d1B2SlUifQ.WJBGn0Iijx2XcwiF2J-RNVcSoe0WCjXeCeUG6zXqsFDaurdQZCjm47Wrx-VFjRgYGVMSYuDDRT7-RBIDLUQrqOAGrcpB8mTacJp5D5SIdRLB4gWi9dQdppdPKsrVkqqAT-1ikA_r8s1pxr5OapDHppX7RtZgAYXHUkihNjknZI1da6ybU44Y2syO5QTbh4PVj0f6u4AfK1jSBeMFAw9kjzmwrMz_3o2wd-I8Qg1vM2OtkMjXhI4nGXypLBq2WYyyZWMeB_7AHHoF880rfWrt--QIHBDOqhW51WW3ovsCCyvOTF2hK1wS4EphUOdXcYaq_T5mUdobKa5RkpmecA-wvA",
+
+    description: "Close-up shot of a kitten yawning. ",
+    views: "3698",
+    user: "Raymond Holt",
+    category: "Cat",
+    src: "https://storage.coverr.co/videos/HYiO01rDqsGLR8zHzB47pHpg6B017wPvJU/preview?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjRDQzlGMzk4NEU5NzdCMkJDQUMwIiwiaWF0IjoxNjQ4MzI1NzgwfQ.uqeLSw6R49jDSO5S76-ITa5xQiPmVGjyHgfK0kRY20A",
+  },
+  {
+    _id: uuid(),
+    title: "Woman and cat near the Christmas tree",
+    thumbnail:
+      "https://image.mux.com/02aQwRpvPIL93jl3z9ewYcFb1kqEkdkZo/thumbnail.jpg?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InQ5UHZucm9ZY0hQNjhYSmlRQnRHTEVVSkVSSXJ0UXhKIn0.eyJ3aWR0aCI6NjQwLCJ0aW1lIjowLCJleHAiOjMxNzE5MjI1MDUzOCwiYXVkIjoidCIsInN1YiI6IjAyYVF3UnB2UElMOTNqbDN6OWV3WWNGYjFrcUVrZGtabyJ9.o3_wViEJaSqPqbcbxH9-9Rwvm8Wq7VhG6_wgYn1A-TjL8FHoZ4rVhF6rgl7jyaB8sk70g9fTbkRSneO9cndPJvwmXxDm7aYzyWwmqXI0mmjRgG8f4X2kjf3KpV_S7nqHhCNMMLtGXHW6pOSryaGnqyZ6qrvePPXxUUX7cTSaEqdOof3LBoLKdWTgzTHTG9REpeEW-HmKSJgkl73Hw73fhRSkL7C_ktafeTrzOUreXKy1EKCeFowY8JZ68dGhnut2nPdmrw1_OTWqv5ZQnsxZ9XgdAB8-d1qnaaiGFJ62WeclrqfUu43VcXfiI_TBBvPx8OpOC7X2os134Ef9Nbu0wg",
+
+    description:
+      "Slow-motion shot of a woman holding her cat near the Christmas tree. ",
+    views: "8524",
+    user: "Oscar Martinez",
+    category: "Cat",
+    src: "https://storage.coverr.co/videos/02aQwRpvPIL93jl3z9ewYcFb1kqEkdkZo/preview?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjRDQzlGMzk4NEU5NzdCMkJDQUMwIiwiaWF0IjoxNjQ4MzI1NzgwfQ.uqeLSw6R49jDSO5S76-ITa5xQiPmVGjyHgfK0kRY20A",
   },
 ];
