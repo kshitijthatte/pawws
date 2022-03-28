c64cc037299d92a0d0129e30acaa208e31ec4b52
Kshitij Thatte <kshitijthatte@gmail.com>
2022-03-28 12:25:35 +0530

    refactor: moved routes to sperate component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import "./styles.css";
-import { Route, Routes } from "react-router-dom";
 import NavDrawer from "./components/NavDrawer";
-import Home from "./pages/Home/";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Playlists from "./pages/Playlists";
-import SinglePlaylist from "./pages/SinglePlaylist";
+import AppRoutes from "./components/AppRoutes";
 
 function App() {
   return (
     <>
       <NavDrawer>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/playlists" element={<Playlists />} />
-          <Route
-            path="/liked"
-            element={<SinglePlaylist playlistTitle="Liked Videos" />}
-          />
-          <Route
-            path="/watchlater"
-            element={<SinglePlaylist playlistTitle="Watch Later" />}
-          />
-        </Routes>
+        <AppRoutes />
       </NavDrawer>
     </>
   );
