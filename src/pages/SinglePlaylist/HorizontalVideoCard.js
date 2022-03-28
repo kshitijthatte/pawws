e12f097b302ee8544864a06f076aae6b64987800
Kshitij Thatte <kshitijthatte@gmail.com>
2022-03-28 12:22:28 +0530

    fix: updated className

--- src/pages/SinglePlaylist/HorizontalVideoCard.js
+++ src/pages/SinglePlaylist/HorizontalVideoCard.js
@@ -1,14 +1,14 @@
 const HorizontalVideoCard = () => {
   return (
-    <div class="card card-badge horizontal-video-card">
-      <div class="card-horizontal">
-        <img class="card-img" src="https://picsum.photos/300/200" />
-        <button class="btn card-icon c-badge-right material-icons">
+    <div className="card card-badge horizontal-video-card">
+      <div className="card-horizontal">
+        <img className="card-img" src="https://picsum.photos/300/200" />
+        <button className="btn card-icon c-badge-right material-icons">
           cancel
         </button>
-        <div class="card-content">
-          <div class="card-title">Our Changing Planet</div>
-          <div class="card-subtitle">by Michael Scarn</div>
+        <div className="card-content">
+          <div className="card-title">Our Changing Planet</div>
+          <div className="card-subtitle">by Michael Scarn</div>
         </div>
       </div>
     </div>
